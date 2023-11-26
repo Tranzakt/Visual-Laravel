2d4ceb12697cede6a89b6940dfc2eeaf8d32c8cb
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2023-11-26 20:54:10 +0000

    Update logo and replace Jetstream logo

--- src/resources/views/components/application-mark.blade.php
+++ src/resources/views/components/application-mark.blade.php
@@ -1,4 +1,14 @@
-<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-  <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-  <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+<svg viewBox="0 0 182 125" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <path
+        d="M 103.7 1.6 L 122.7 12.7 L 122.7 56.5 L 141.7 45.4 L 141.8 23.5 L 160.8 12.5 L 179.7 23.5 L 179.8 45.4 L 160.8 56.4 L 160.8 78.3 L 122.7 100.4 L 84.7 78.4 L 84.8 12.7 Z"
+        style="fill:white;stroke:#ff2d20;stroke-width:4;stroke-linecap:round;stroke-linejoin:round" />
+    <path
+        d="M 122.8 78.3 L 122.7 100.4 M 141.8 23.5 L 160.8 34.5 M 141.7 45.4 L 160.8 56.4 M 84.8 12.7 L 103.8 23.7 L 103.7 67.5 L 122.8 78.3 L 160.8 56.4 L 160.8 34.5 L 179.7 23.5 M 103.7 67.5 L 122.7 56.5 M 103.8 23.7 L 122.7 12.7"
+        style="fill:none;stroke:#ff2d20;stroke-width:4;stroke-linecap:round;stroke-linejoin:round" />
+    <path
+        d="M 1.6 57.2 L 20.6 46.1 L 39.7 57.2 L 39.7 35.3 L 58.7 24.3 L 77.8 35.3 L 72.9 101.0 L 34.4 123.12 L 15.1 112.1 Z"
+        style="fill:white;stroke:#ff2d20;stroke-width:4;stroke-linecap:round;stroke-linejoin:round" />
+    <path
+        d="M 46.4 83.4 L 55.8 88.8 M 39.7 35.3 L 58.8 46.4 M 1.6 57.2 L 20.7 68.2 L 39.7 57.2 L 48.7 92.9 L 55.8 88.8 L 58.8 46.4 L 77.8 35.3 M 20.7 68.2 L 34.4 123.12"
+        style="fill:none;stroke:#ff2d20;stroke-width:4;stroke-linecap:round;stroke-linejoin:round" />
 </svg>
