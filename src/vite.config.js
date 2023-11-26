f4ede8db37ae516136ca078fab0c67f95ebbbac6
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2023-11-26 20:52:20 +0000

    Include vite Tailwind plugin

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { TailwindCSSVitePlugin } from 'tailwindcss-vite-plugin';
 
 export default defineConfig({
     plugins: [
@@ -13,5 +14,6 @@ export default defineConfig({
                 'app/Livewire/**',
             ],
         }),
+        TailwindCSSVitePlugin(),
     ],
 });
