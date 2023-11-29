70aaa786b8a91bf999ab1ff264c8bf8c9c88f38e
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2023-11-29 16:46:42 +0000

    Fix rector config

--- src/rector.php
+++ src/rector.php
@@ -9,7 +9,7 @@ use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__.'/src',
+        __DIR__,
     ]);
 
     $rectorConfig->rules([
