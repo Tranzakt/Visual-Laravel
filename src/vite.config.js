af6789fe070c89039ea6e607ed7a8e22cbe50cf3
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2025-01-03 20:26:42 +0000

    Fix Github actions

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,19 +1,26 @@
 import { defineConfig } from 'vite'
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 import laravel, { refreshPaths } from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['VisualLaravel/Core/resources/css/app.css', 'VisualLaravel/Core/resources/js/app.js'],
             refresh: [
                 ...refreshPaths,
-                'VisualLaravel/core/Filament/**',
-                'VisualLaravel/core/Forms/Components/**',
-                'VisualLaravel/core/Livewire/**',
-                'VisualLaravel/core/Infolists/Components/**',
-                'VisualLaravel/core/Providers/Filament/**',
-                'VisualLaravel/core/Tables/Columns/**',
+                'VisualLaravel/Base/Filament/**',
+                'VisualLaravel/Base/Providers/Filament/**',
+                'VisualLaravel/Core/Forms/Components/**',
+                'VisualLaravel/Core/Livewire/**',
+                'VisualLaravel/Core/Infolists/Components/**',
+                'VisualLaravel/Core/Tables/Columns/**',
             ],
+        }),
+        // Put the Codecov vite plugin after all other plugins
+        codecovVitePlugin({
+          enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+          bundleName: "Visual-Laravel-Vite-Bundle",
+          uploadToken: process.env.CODECOV_TOKEN,
         }),
     ],
 })
